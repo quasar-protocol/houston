55eccd5f6e7adf67900b3e949e826793c37fdea4
Hyungsuk Kang <hskang9@gmail.com>
2020-10-17 01:25:07 +0900

    Update postinstall.sh

--- packages/core/scripts/postinstall.sh
+++ packages/core/scripts/postinstall.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+# Set access to global node_modules to the current user
+# Mac OS
+[ -d "/usr/local/lib/node_modules" ] && 
+chown -R $USER /usr/local/lib/node_modules 
+# Linux
+[ -d "/usr/lib/node_modules" ] && 
+chown -R $USER /usr/lib/node_modules
 # Install Rust
-# TODO: fix rust install bug problem
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # Set Rust PATH
 source $HOME/.cargo/env
